b81c1559765428f1e997de7c60ff98fff74946e3
Nahuel Clauser <nahuelclauser@gmail.com>
2021-01-12 14:08:37 -0300

    Form

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
+const {Engine, Render, Runner, World, Bodies} = Matter;
 
+const width = 600;
+const height = 600;
 const engine = Engine.create();
 
 const {world}= engine;
@@ -8,36 +10,27 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        width,
+        height
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}));
-// const shape = Bodies.rectangle(200,200,50,50,{
-//     isStatic: true
-// });
-// World.add(world, shape);
+
 
 //Walls
 
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true}),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true}),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true})
+    Bodies.rectangle(width/2, 0, width, 40, { isStatic: true}),
+    Bodies.rectangle(width/2, height, width, 40, { isStatic: true}),
+    Bodies.rectangle(0, height/2, 40, height, { isStatic: true}),
+    Bodies.rectangle(width, height/2, 40, height, { isStatic: true})
 
 
 ];
 
 World.add(world, walls);
 
-//RandomShapes
-for(let i=0; i<20; i++){
-    World.add(world, Bodies.rectangle(200, 200, 50, 50));
-}
+
 
