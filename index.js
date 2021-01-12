3be0d42d40ce25a45ad08fe21eb991db7627737f
Nahuel Clauser <nahuelclauser@gmail.com>
2021-01-12 12:40:08 -0300

    Formas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Engine, Render, Runner, World, Bodies} = Matter;
+const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
 
 const engine = Engine.create();
 
@@ -15,7 +15,29 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200,200,50,50,{
-    isStatic: true
-});
-World.add(world, shape);
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}));
+// const shape = Bodies.rectangle(200,200,50,50,{
+//     isStatic: true
+// });
+// World.add(world, shape);
+
+//Walls
+
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, { isStatic: true}),
+    Bodies.rectangle(400, 600, 800, 40, { isStatic: true}),
+    Bodies.rectangle(0, 300, 40, 600, { isStatic: true}),
+    Bodies.rectangle(800, 300, 40, 600, { isStatic: true})
+
+
+];
+
+World.add(world, walls);
+
+//RandomShapes
+for(let i=0; i<20; i++){
+    World.add(world, Bodies.rectangle(200, 200, 50, 50));
+}
+
