c3ec760d4f700a8d2abf8deeb8a03fc764fce0c5
Nahuel Clauser <nahuelclauser@gmail.com>
2021-02-19 19:16:49 -0300

    Making maze

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const {Engine, Render, Runner, World, Bodies} = Matter;
 
-const cells= 3;
+const cells= 9;
 const width = 600;
 const height = 600;
 const engine = Engine.create();
+
+const unitLength = width / cells;
+//const unitWidth = width / cells;
 
 const {world}= engine;
 
@@ -112,5 +115,43 @@ const stepThroughCell = (row, column) =>{
 
 };
 
-//stepThroughCell(startRow,startColumn);
-stepThroughCell(1,1);
+stepThroughCell(startRow,startColumn);
+
+horizontals.forEach((row, rowIndex) =>{
+    row.forEach((open, columnIndex) => {
+        if(open){
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength/2,
+            rowIndex*unitLength+unitLength,
+            unitLength,
+            10,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world,wall);
+    });
+});
+
+verticals.forEach((row,rowIndex) => {
+    row.forEach((open, columnIndex) =>{
+        if (open){
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength /2,
+            10,
+            unitLength,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world,wall);
+    });
+});
+//stepThroughCell(1,1);
