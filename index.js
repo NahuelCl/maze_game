3557f2c927893b1959bf3b89c72fa5ca65c5f016
Nahuel Clauser <nahuelclauser@gmail.com>
2021-02-20 18:16:15 -0300

    Moving and winning

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const {Engine, Render, Runner, World, Bodies} = Matter;
+const {Engine, Render, Runner, World, Bodies, Body, Events} = Matter;
 
-const cells= 9;
+const cells= 3;
 const width = 600;
 const height = 600;
 const engine = Engine.create();
@@ -9,6 +9,7 @@ const unitLength = width / cells;
 //const unitWidth = width / cells;
 
 const {world}= engine;
+engine.world.gravity.y=0;
 
 const render = Render.create({
     element: document.body,
@@ -26,10 +27,10 @@ Runner.run(Runner.create(), engine);
 //Walls
 
 const walls = [
-    Bodies.rectangle(width/2, 0, width, 40, { isStatic: true}),
-    Bodies.rectangle(width/2, height, width, 40, { isStatic: true}),
-    Bodies.rectangle(0, height/2, 40, height, { isStatic: true}),
-    Bodies.rectangle(width, height/2, 40, height, { isStatic: true})
+    Bodies.rectangle(width/2, 0, width, 2, { isStatic: true}),
+    Bodies.rectangle(width/2, height, width, 2, { isStatic: true}),
+    Bodies.rectangle(0, height/2, 2, height, { isStatic: true}),
+    Bodies.rectangle(width, height/2, 2, height, { isStatic: true})
 
 
 ];
@@ -154,4 +155,58 @@ verticals.forEach((row,rowIndex) => {
         World.add(world,wall);
     });
 });
-//stepThroughCell(1,1);
+//Goal
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * .7,
+    unitLength * .7,
+    {
+        label: 'goal',
+        isStatic : true
+    }
+);
+World.add(world, goal);
+
+//Ball
+
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 4,
+    {
+        label: 'ball'
+    }
+);
+
+World.add(world, ball);
+
+document.addEventListener('keydown', event => {
+    const { x,y } = ball.velocity;
+    console.log(x,y);
+    if (event.keyCode === 87){
+        Body.setVelocity(ball, {x, y: y-5});
+    }
+
+    if (event.keyCode === 68){
+        Body.setVelocity(ball, {x: x+5, y});
+    }
+
+    if (event.keyCode === 83){
+        Body.setVelocity(ball, {x, y: y+5});
+    }
+
+    if (event.keyCode === 65){
+        Body.setVelocity(ball, {x : x-5, y});
+    }
+});
+
+Events.on(engine, 'collisionStart', event =>{
+    event.pairs.forEach((collision) => {
+            const labels = ['ball','goal'];
+
+            if(labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)){
+                console.log('User won!!');
+            }
+    });
+});
